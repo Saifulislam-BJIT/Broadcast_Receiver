6bfb68de1c19adfa2c3ce6d699438551c4a43efa
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-29 01:39:45 +0600

    internet check

--- app/src/main/java/com/saiful/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/saiful/broadcastreceiver/MainActivity.kt
@@ -2,15 +2,20 @@ package com.saiful.broadcastreceiver
 
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import com.saiful.broadcastreceiver.broadcast.AirplaneModeChangeReceiver
 import com.saiful.broadcastreceiver.broadcast.BatteryStatus
+import com.saiful.broadcastreceiver.broadcast.Internet
 
 class MainActivity : AppCompatActivity() {
     lateinit var airplaneModeChangeReceiver: AirplaneModeChangeReceiver
     lateinit var batteryStatus: BatteryStatus
+    lateinit var internet: Internet
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,10 +31,17 @@ class MainActivity : AppCompatActivity() {
         IntentFilter(Intent.ACTION_BATTERY_CHANGED).also {
             registerReceiver(batteryStatus, it)
         }
+
+        internet = Internet()
+        val filter = IntentFilter()
+        filter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
+        registerReceiver(internet, filter)
     }
 
     override fun onStop() {
         super.onStop()
         unregisterReceiver(airplaneModeChangeReceiver)
+        unregisterReceiver(batteryStatus)
+        unregisterReceiver(internet)
     }
 }
---
+++ app/src/main/java/com/saiful/broadcastreceiver/broadcast/Internet.kt
@@ -0,0 +1,42 @@
+package com.saiful.broadcastreceiver.broadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+
+class Internet : BroadcastReceiver() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        if(isOnline(context!!)) {
+            Log.d("Internet", "onReceive: internet is available")
+        } else {
+            Log.d("Internet", "onReceive: Internet is not available")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun isOnline(context: Context): Boolean {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_CELLULAR")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_WIFI")
+                return true
+            } else if (capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+                Log.i("Internet", "NetworkCapabilities.TRANSPORT_ETHERNET")
+                return true
+            }
+        }
+        return false
+    }
+}
