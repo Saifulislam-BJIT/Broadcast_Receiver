db7eb3d8c4b360f93788b0541135236bd527ffc0
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-29 12:45:50 +0600

    battery

--- app/src/main/java/com/saiful/broadcastreceiver/broadcast/BatteryStatus.kt
+++ app/src/main/java/com/saiful/broadcastreceiver/broadcast/BatteryStatus.kt
@@ -4,37 +4,41 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.BatteryManager
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 
 class BatteryStatus : BroadcastReceiver() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onReceive(context: Context?, intent: Intent?) {
+        Log.d("battery", "-------- START --------")
         val status: Int = intent?.getIntExtra(BatteryManager.EXTRA_STATUS, -1) ?: -1
-        Log.d("TAG", "onReceive: battery Status: $status")
+        Log.d("battery", "battery Status: $status")
         val scale = intent?.getIntExtra(BatteryManager.EXTRA_SCALE, -1) ?: -1
         val level = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) ?: -1
-        Log.d("TAG", "onReceive: battery scale: $scale")
-        Log.d("TAG", "onReceive: battery level: $level")
-        Log.d("TAG", "onReceive: battery percentage: ${level * 100 / scale.toFloat()}")
-        Log.d("TAG", "onReceive: ----------------")
+        Log.d("battery", "battery scale: $scale")
+        Log.d("battery", "battery level: $level")
+        Log.d("battery", "battery percentage: ${level * 100 / scale.toFloat()}")
+        Log.d("battery", "----------------")
         val isCharging =
             status == BatteryManager.BATTERY_STATUS_CHARGING || status == BatteryManager.BATTERY_STATUS_FULL
-        Log.d("TAG", "onReceive: battery is changing: $isCharging")
-        Log.d("TAG", "onReceive: battery is BatteryManager.BATTERY_STATUS_FULL: ${BatteryManager.BATTERY_STATUS_FULL}")
-        Log.d("TAG", "onReceive: battery BatteryManager.BATTERY_STATUS_CHARGING: ${BatteryManager.BATTERY_STATUS_CHARGING}")
-        Log.d("TAG", "onReceive: ----------------")
+        Log.d("battery", "battery is changing: $isCharging")
+        Log.d("battery", "battery is BatteryManager.BATTERY_STATUS_FULL: ${BatteryManager.BATTERY_STATUS_FULL}")
+        Log.d("battery", "battery BatteryManager.BATTERY_STATUS_CHARGING: ${BatteryManager.BATTERY_STATUS_CHARGING}")
+        Log.d("battery", "----------------")
 
         val chargePlug: Int = intent?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1) ?: -1
-        Log.d("TAG", "onReceive: battery charge plug: $chargePlug")
-        Log.d("TAG", "onReceive: ----------------")
+        Log.d("battery", "battery charge plug: $chargePlug")
+        Log.d("battery", "----------------")
 
         val usbCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
-        Log.d("TAG", "onReceive: battery usb change: $usbCharge")
-        Log.d("TAG", "onReceive: ----------------")
+        Log.d("battery", "battery usb change: $usbCharge")
+        Log.d("battery", "----------------")
         val acCharge: Boolean = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
-        Log.d("TAG", "onReceive: battery ac charge: $acCharge")
-        Log.d("TAG", "onReceive: ----------------")
+        Log.d("battery", "battery ac charge: $acCharge")
+        Log.d("battery", "----------------")
         val batteryPorperty = BatteryManager.BATTERY_PROPERTY_STATUS
-        Log.d("TAG", "onReceive: battery property: $batteryPorperty")
-
+        Log.d("battery", "battery property: $batteryPorperty")
+        Log.d("battery", "-------- END --------")
     }
 }
