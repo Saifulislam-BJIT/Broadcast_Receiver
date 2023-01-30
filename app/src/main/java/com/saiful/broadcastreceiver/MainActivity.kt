1c46f1032a021cda749e54540c4323b477e12344
Saiful Islam <saiful.islam@bjitacademy.com>
2023-01-30 11:42:15 +0600

    smsbradcast

--- app/src/main/java/com/saiful/broadcastreceiver/MainActivity.kt
+++ app/src/main/java/com/saiful/broadcastreceiver/MainActivity.kt
@@ -1,21 +1,25 @@
 package com.saiful.broadcastreceiver
 
+import android.Manifest
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.saiful.broadcastreceiver.broadcast.AirplaneModeChangeReceiver
 import com.saiful.broadcastreceiver.broadcast.BatteryStatus
 import com.saiful.broadcastreceiver.broadcast.Internet
-import com.saiful.broadcastreceiver.broadcast.SMSBroadcastReceiver
+
+private const val MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 1
 
 class MainActivity : AppCompatActivity() {
     lateinit var airplaneModeChangeReceiver: AirplaneModeChangeReceiver
     lateinit var batteryStatus: BatteryStatus
     lateinit var internet: Internet
-    lateinit var smsBroadcastReceiver: SMSBroadcastReceiver
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,16 +41,24 @@ class MainActivity : AppCompatActivity() {
         filter.addAction("android.net.conn.CONNECTIVITY_CHANGE")
         registerReceiver(internet, filter)
 
-        smsBroadcastReceiver = SMSBroadcastReceiver()
-        filter.addAction("android.permission.RECEIVE_SMS")
-        registerReceiver(smsBroadcastReceiver, filter)
-    }
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECEIVE_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECEIVE_SMS),
+                MY_PERMISSIONS_REQUEST_RECEIVE_SMS
+            )
+        }
 
+
+    }
     override fun onStop() {
         super.onStop()
         unregisterReceiver(airplaneModeChangeReceiver)
         unregisterReceiver(batteryStatus)
         unregisterReceiver(internet)
-        unregisterReceiver(smsBroadcastReceiver)
     }
 }
--- app/src/main/java/com/saiful/broadcastreceiver/broadcast/SMSBroadcastReceiver.kt
+++ app/src/main/java/com/saiful/broadcastreceiver/broadcast/SMSBroadcastReceiver.kt
@@ -5,10 +5,18 @@ import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
 import android.util.Log
+import android.widget.Toast
 
-class SMSBroadcastReceiver: BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
+
+class SMSBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d("TAG", "sms receive: ----------");
+        if (!intent.action.equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) return
         val extractMessages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
-        extractMessages.forEach { smsMessage -> Log.v("SMS", smsMessage.displayMessageBody) }
+        extractMessages.forEach {
+            Toast.makeText(context, it.displayMessageBody, Toast.LENGTH_SHORT).show()
+            intent.putExtra("sms", it.displayMessageBody)
+            Log.d("TAG", "tag: ${it.displayMessageBody}");
+        }
     }
 }
